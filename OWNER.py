9a2491dd686ba09a87422c9f9074b67259bdee66
thaker7 <166921231+thaker7@users.noreply.github.com>
2024-04-22 01:33:41 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
 OWNER = ["G_7_Rr"]
 OWNER_NAME = "teto"
-BOT_TOKEN = "6432841340:AAFmzvjpBRc025leCsXzqafV7N1TLT_VLCc"
+BOT_TOKEN = "7057013591:AAHuL-0VUmRSIXEWYiY932p9iHZknCrRjX4"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/CH_NR"
-GROUP = "https://t.me/t7_AU"
+GROUP = "https://t.me/ffrfg"
 VIDEO = "https://graph.org/file/061951daec85b0f94b83c.jpg"
 LOGS = "zombie2"
 
